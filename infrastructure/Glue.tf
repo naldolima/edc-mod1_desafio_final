9eebec83fd1718ba7030ab41a49221865afe3ec0
Naldo Lima <francinaldolima6@gmail.com>
2021-08-19 13:54:36 -0300

    ajuste name job 1

--- infrastructure/Glue.tf
+++ infrastructure/Glue.tf
@@ -1,6 +1,6 @@
 resource "aws_glue_job" "job-censo" {
-  name     = "jobcenso"
-  role_arn = aws_iam_role.jobcenso.arn
+  name     = "job-censo"
+  #role_arn = aws_iam_role.job-censo.arn
 
   command {
     script_location = "s3://code/pyspark/glue_spark_job.py"
