699c1d3e7a1a83b1c84b271e3ef9940715a509f3
Naldo Lima <francinaldolima6@gmail.com>
2021-08-19 15:10:34 -0300

    alterações no glue

--- infrastructure/Glue.tf
+++ infrastructure/Glue.tf
@@ -2,19 +2,14 @@ resource "aws_glue_job" "job_censo_new" {
   name         = "job_name_new"
   description  = "job-desc"
   role_arn = aws_iam_policy.glue_policy.arn
+  glue_version = "2.0"
+  worker_type  = "G.1X"
 
   command {
     script_location = "s3://datalake-naldolima-edc-tf/code/pyspark/glue_spark_job.py"
     python_version  = "3"
   }
 
-  default_arguments = {    
-    "--job-language"          = "python"
-    "--ENV"                   = "env"
-    "--spark-event-logs-path" = "s3://datalake-naldolima-edc-tf/code/pyspark/"
-    "--job-bookmark-option"   = "job-bookmark-enable"
-    "--enable-spark-ui"       = "true"
-  }
 
    execution_property {
     max_concurrent_runs = 1
