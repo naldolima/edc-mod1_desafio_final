2a194bdc5faa99fd9135e4b82f5a3d830b64a8ac
Naldo Lima <francinaldolima6@gmail.com>
2021-08-19 14:12:20 -0300

    ajuste policy glue

--- infrastructure/Glue.tf
+++ infrastructure/Glue.tf
@@ -1,6 +1,7 @@
 resource "aws_glue_job" "job-censo" {
   name     = "job-censo"
-  role_arn = "${aws_iam_role.service-name.arn}"
+  role_arn = aws_iam_policy.glue_policy.arn
+
 
   command {
     script_location = "s3://code/pyspark/glue_spark_job.py"
